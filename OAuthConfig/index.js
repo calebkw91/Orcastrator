c70f16ee2c5981a71ff815fef30b105131401b82
vfavorito <vince.favorito@gmail.com>
2021-03-06 07:15:29 -0600

    deleted some of the react template stuff and started configuring oauth strategies

---
+++ OAuthConfig/index.js
@@ -0,0 +1,20 @@
+const Google = {
+    clientID: "something",
+    clientSecret: "something"
+}
+
+const Facebook = {
+    clientID: "something",
+    clientSecret: "something"
+}
+
+const Github = {
+    clientID: "something",
+    clientSecret: "something"
+}
+
+module.exports = {
+    Google,
+    Facebook,
+    Github
+}
