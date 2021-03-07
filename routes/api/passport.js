f253667e2d61e8ba23dfaf541f36e2c55e98d403
vfavorito <vince.favorito@gmail.com>
2021-03-07 11:07:48 -0600

    changed the redirect page to userhomepage instead of landing page after authentication

--- routes/api/passport.js
+++ routes/api/passport.js
@@ -9,7 +9,7 @@ passportRouter.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/login' }),
     function (req, res) {
         // Successful authentication, redirect home.
-        res.redirect("http://localhost:3000/");
+        res.redirect("http://localhost:3000/User");
         // window.location.href="/"
 });
 
@@ -20,7 +20,7 @@ passportRouter.get("/auth/github",
 // if github login fails return to "/login" otherwise if login succeeds go to "/"
 passportRouter.get("/auth/github/callback",
     passport.authenticate("github", { failureRedirect: "/login" }), (req, res) => {
-        res.redirect("http://localhost:3000/");
+        res.redirect("http://localhost:3000/User");
     }
 );
 
