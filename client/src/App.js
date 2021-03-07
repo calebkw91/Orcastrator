e253fea88a37cca956910f467d428664cfec34e3
vfavorito <vince.favorito@gmail.com>
2021-03-07 10:56:15 -0600

    created a login component to initiate Oauth

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,33 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import UserHomepage from "./components/UserHomepage";
 import PodDisplay from "./components/PodDisplay";
 import Landing from "./components/Landing";
+import Login from "./components/Login/login"
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-          <Switch>
-              <Route exact path="/">
-                <Landing />
-              </Route>
-              <Route exact path="/User">
-                <UserHomepage />
-              </Route>
-              <Route exact path="/Pod/:id">
-                <PodDisplay />
-              </Route>
-              <Route path="*">
-                <Landing />
-              </Route>
-          </Switch>
-        </div>
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/User">
+            <UserHomepage />
+          </Route>
+          <Route exact path="/Pod/:id">
+            <PodDisplay />
+          </Route>
+          <Route path="*">
+            <Landing />
+          </Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
