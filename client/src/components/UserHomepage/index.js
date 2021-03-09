db3ac85ecf209c0be4eefd5d89cdd11517e5fe01
vfavorito <vince.favorito@gmail.com>
2021-03-08 21:30:02 -0600

    fixing authentication bugs and redirects

--- client/src/components/UserHomepage/index.js
+++ client/src/components/UserHomepage/index.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 
-function UserHomepage(props){
-    return(
+
+function UserHomepage(props) {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        axios.get("/User")
+            .then((res) => {
+                console.log(res);
+                setUser(res);
+            })
+            .catch(err => console.log(err))
+    }, []);
+
+    return (
+        <div>
         <h1>UserHomepage Page</h1>
+        {/* <h1>{user.data.displayName}</h1> */}
+        </div>
     )
+
 }
 
 
