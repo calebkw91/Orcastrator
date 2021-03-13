aa0ecdbfd70d53a5b4fa4ae12f4c636faa36f6aa
Daron Schmit <darons23@gmail.com>
2021-03-13 14:21:03 -0600

    got create group button working

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -3,27 +3,8 @@ import CreateGroupButton from "../../components/CreateGroupButton";
 import Footer from "../../components/Footer";
 import GroupInfo from "../../components/GroupInfo";
 import UserInfo from "../../components/UserInfo";
-import GroupAdd from "../../components/GroupAdd";
-import { Button } from "react-bootstrap";
-import API from "../../utils/API";
 
 function Dashboard(props) {
-    const [modalShow, setModalShow] = React.useState(false);
-
-    const handleFormSubmit = event => {
-        event.preventDefault();
-
-        let newGroup = {
-            name: event.target.form[0].value,
-            members: []
-        }
-        
-        API.saveGroup(newGroup)
-            .then(console.log("group saved"))
-            .catch(err => console.log(err));
-
-        setModalShow(false);
-    }
 
     return (
         <div>
@@ -33,7 +14,6 @@ function Dashboard(props) {
             <div className="container">
             {/* user info in the navbar */}
             {/* display group info on page */}
-<<<<<<< HEAD
                 <div className="row">
                     <CreateGroupButton />
                     <GroupInfo />
@@ -42,24 +22,6 @@ function Dashboard(props) {
             {/* later - display other groups */}
             </div>
         </div> 
-=======
-            <div className="row">
-                <CreateGroupForm />
-                <GroupInfo />
-                <Button variant="primary" onClick={() => setModalShow(true)}>
-                    Add Group
-                </Button>
-
-                <GroupAdd
-                    show={modalShow}
-                    onHide={() => setModalShow(false)}
-                    handleFormSubmit={handleFormSubmit}
-                />
-            </div>
-            {/* display group making form on page - later put into a separate tab/page/whatever*/}
-            {/* later - display other groups */}
-        </div>
->>>>>>> 93405f8df987a83ad590ed1aad65e95e90448427
     );
 }
 
