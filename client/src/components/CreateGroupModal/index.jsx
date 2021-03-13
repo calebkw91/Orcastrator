5c74a4b84286963ba1554e360474f5698f8e3801
Daron Schmit <darons23@gmail.com>
2021-03-13 12:59:02 -0600

    kind of finished modal

--- client/src/components/CreateGroupModal/index.jsx
+++ client/src/components/CreateGroupModal/index.jsx
@@ -1,33 +1,49 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-function CreateGroupModal(props){
-    return(
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Modal heading
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-            <h4>Centered Modal</h4>
-            <p>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                consectetur ac, vestibulum at eros.
-            </p>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    )
+function CreateGroupModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Create New Pod
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form.Group>
+          <div className="container">
+            <div className="row">
+              <Form.Control size="lg" type="text" placeholder="Pod Name" />
+            </div>
+            <br />
+            <div className="row">
+              <Form.Control type="text" placeholder="Pod Description" />
+            </div>
+            <br />
+            <div className="row">
+                <div className="col-6">
+                    <Form.Control size="sm" type="text" placeholder="Member Name" />
+                </div>
+                <div className="col-6">
+                    <Form.Control size="sm" type="text" placeholder="Role" />
+                </div>
+            </div>
+          </div>
+        </Form.Group>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button>Save</Button>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default CreateGroupModal;
