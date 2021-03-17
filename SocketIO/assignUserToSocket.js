1977500b8ec29bf74f8aefd4d10c100c8001de61
ricky <rickycohen88@gmail.com>
2021-03-17 11:58:32 -0500

    adding pod to socket in asign to user function

--- SocketIO/assignUserToSocket.js
+++ SocketIO/assignUserToSocket.js
@@ -2,12 +2,14 @@
 function assignUserToSocket(socket){
     const username = socket.handshake.auth.username;
     const userID = socket.handshake.auth.userID;
+    const pod = socket.handshake.auth.podID
     console.log(socket.handshake.auth);
     // if(!username||!userID){
     //   return next(new Error("Invalid Username"));
     // }
     socket.username = username;
     socket.userID = userID;
+    socket.pod = pod;
     console.log(socket);
     return socket
 }
