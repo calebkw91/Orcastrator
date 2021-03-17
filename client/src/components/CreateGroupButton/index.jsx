34d545ef42aafe91ae30a2f43d700aefff825460
Daron Schmit <darons23@gmail.com>
2021-03-16 23:05:22 -0500

    got rid of unnecessary import in creategroupbutton

--- client/src/components/CreateGroupButton/index.jsx
+++ client/src/components/CreateGroupButton/index.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useContext} from "react";
 import Button from "react-bootstrap/Button"
 import CreateGroupModal from "../CreateGroupModal";
 import API from "../../utils/API";
