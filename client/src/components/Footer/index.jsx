09856788a1a7bbb7622135bee6a90518d27caa07
Daron Schmit <darons23@gmail.com>
2021-03-17 17:27:27 -0500

    added placeholder images for footer animation

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import footer from "./footer-image.png";
+import frontFooter from "./footer-image-front.png";
+import rearFooter from "./footer-image-rear.png";
 import "./style.css"
 
 
 
 function Footer({ children }) {
     return (
-        <div className="footer" fixed="bottom">
-            <img alt="Orca Breaching" src={footer}></img>
+        <div fixed="bottom">
+            <img className="footer rear" alt="waves" src={rearFooter}></img>
+            <img className="footer middle" alt="Orca Breaching" src={footer}></img>
+            <img className="footer front" alt="waves" src={frontFooter}></img>
         </div>
     )
 }
