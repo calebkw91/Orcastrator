34a177b55c9658c276b28b4dc9e3b09395514ae8
Daron Schmit <darons23@gmail.com>
2021-03-17 18:10:51 -0500

    added animation to footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -2,18 +2,46 @@ import React from "react";
 import footer from "./footer-image.png";
 import frontFooter from "./footer-image-front.png";
 import rearFooter from "./footer-image-rear.png";
-import "./style.css"
-
-
+import ReactAnime from "react-animejs";
+import "./style.css";
 
 function Footer({ children }) {
-    return (
-        <div fixed="bottom">
-            <img className="footer rear" alt="waves" src={rearFooter}></img>
-            <img className="footer middle" alt="Orca Breaching" src={footer}></img>
-            <img className="footer front" alt="waves" src={frontFooter}></img>
-        </div>
-    )
+  const { Anime, stagger } = ReactAnime;
+  return (
+    <div>
+      <Anime
+        initial={[
+          {
+            targets: ".rear",
+            translateY: 10,
+            loop: true,
+            direction: "alternate",
+            easing: "easeOutInSine",
+            delay: 500,
+            duration: 3000
+          },
+        ]}
+      >
+        <img className="footer rear" alt="waves" src={rearFooter}></img>
+      </Anime>
+      <img className="footer middle" alt="Orca Breaching" src={footer}></img>
+      <Anime
+        initial={[
+          {
+            targets: ".front",
+            translateY: 10,
+            loop: true,
+            direction: "alternate",
+            easing: "easeOutInSine",
+            duration: 3000,
+            delay: stagger(500)
+          },
+        ]}
+      >
+        <img className="footer front" alt="waves" src={frontFooter}></img>
+      </Anime>
+    </div>
+  );
 }
 
 export default Footer;
