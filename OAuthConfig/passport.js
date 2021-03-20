0e5437e1fb3248fe677f42da7fb7c9d6699821b7
calebkw <walke1291@gmail.com>
2021-03-20 11:35:58 -0500

    oauth path changes

--- OAuthConfig/passport.js
+++ OAuthConfig/passport.js
@@ -18,7 +18,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GoogleID,
     clientSecret: process.env.GoogleSecret,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://orcastrator.herokuapp.com/auth/google/callback"
 },
     function (accessToken, refreshToken, profile, cb) {
 
@@ -51,7 +51,7 @@ passport.use(new GoogleStrategy({
 passport.use(new GithubStrategy({
     clientID: process.env.GithubID,
     clientSecret: process.env.GithubSecret,
-    callbackUrl: "/auth/github/callback"
+    callbackUrl: "https://orcastrator.herokuapp.com/auth/github/callback"
 },
     function (accessToken, refreshToken, profile, cb) {
         // User.findOrCreate({ googleId: profile.id }, function (err, user) {
